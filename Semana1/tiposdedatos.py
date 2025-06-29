a18e8c6a2db380f1f4740dfb6364dc19160f0853
Marcelo <marcelomunozp09@gmail.com>
2025-06-29 18:17:15 -0400

    Tipos de datos Semana1

---
+++ Semana1/tiposdedatos.py
@@ -0,0 +1,8 @@
+print ('Prueba de tipos de datos')
+
+print(type("Hello World"))
+print(type(17))
+print(type(3.2))
+print(type("17"))  #Al estar en comillas pasa a ser string/str
+
+print(1,000,000) #Al tener la , python interpreta como una lista de 3 números
