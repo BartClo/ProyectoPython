908a9f31ff1dfb0432d35d1141b88a5a73d0a575
Marcelo <marcelomunozp09@gmail.com>
2025-06-29 19:37:06 -0400

    Conversión datos Semana1

---
+++ Semana1/conversion.py
@@ -0,0 +1,16 @@
+print("Conversión de datos (typecasting)")
+
+
+name = "Marcelo Muñoz" #string
+age = 21 #int 
+prom = 5.6 #float
+is_student = True  #boolean
+
+prom = int(prom)
+print(prom)
+
+age = float(age)
+print(age)
+
+age = str(age)
+print(age)
