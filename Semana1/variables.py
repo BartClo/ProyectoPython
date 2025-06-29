050b2e06d9fcae572600fec0bbedbc5bd7c30ce3
Marcelo <marcelomunozp09@gmail.com>
2025-06-29 18:29:38 -0400

    Variables Semana1

---
+++ Semana1/variables.py
@@ -0,0 +1,12 @@
+print("Variables")
+
+#La entencia de asignación crea nuevas variables y les asigna un valor
+mensaje = "Hola Mundo"
+n = 17
+pi = 3.14159
+
+print(mensaje)
+print(n)
+print(pi)
+print("El nombre de las variables debe empezar con una letra, Python distingue mayusculas y sin carácteres como $ y sin palabras reservadas")
+print("Algunas Palabras reservadas de Python son: and, as, assert, break, class, continue, def, del, elif, else, except, False, finally, for, from, global, if, import, in, is, lambda, None, nonlocal, not, or, pass, raise, return, True, try, while with")
