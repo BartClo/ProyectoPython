8cb221d94a1a23ce513d64683451d184640613f0
Marcelo <marcelomunozp09@gmail.com>
2025-07-06 17:31:01 -0400

    Semana2 Python

---
+++ Semana2/calcular_area_rectangulo.py
@@ -0,0 +1,8 @@
+print("Bienvenido a la calculadora de área de rectangulo!!")
+
+largo = float(input("¿Cuántos cm de largo tiene el rectangulo?: "))
+alto = float(input("¿Cuántos cm de alto tiene el rectangulo?: "))
+
+area = largo * alto
+
+print(f"El área del rectangulo es {area}cm**2")
---
+++ Semana2/input.py
@@ -0,0 +1,9 @@
+nombre = input("¿Cómo te llamas?: ")
+edad = int(input("¿Cuántos años tienes?: "))
+
+
+#edad = int(edad)
+edad = edad + 1
+
+print(f"Hola {nombre}!")
+print(f"Tienes {edad} años")
