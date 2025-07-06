94563bf955cb68ffa278befcc221bfc271161f04
Marcelo <marcelomunozp09@gmail.com>
2025-07-06 17:59:04 -0400

    Semana2 CalcularCircunferencia

---
+++ Semana2/calcular_circunferencia_circulo.py
@@ -0,0 +1,10 @@
+import math
+
+#2*pi*r
+
+print("Bievenido a la calculadora de la circunferencia de un círculo!!")
+
+radio = float(input("Radio del círculo: "))
+circunferencia= 2 * math.pi * radio
+
+print(f"La circunferencia es de {round(circunferencia, 2)} cm") #redondea y deja 2 núm de decimal
