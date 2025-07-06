fdb146a10bf821e1bd46e7d6e27686ecdb830087
Marcelo <marcelomunozp09@gmail.com>
2025-07-06 17:53:10 -0400

    Semana2 LibreriaMath

---
+++ Semana2/libreria_math.py
@@ -0,0 +1,12 @@
+import math
+
+x = 9.6
+
+print(math.pi)
+print(math.e)
+
+#resultado = math.sqrt(x) #Raíz cuadrada
+#resultado = math.ceil(x) #Redondea para arriba
+resultado = math.floor(x) #Redondea para abajo
+
+print(resultado)
