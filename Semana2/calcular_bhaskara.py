8881b7da5e8ce58e4e4f9a1449089694c25504e4
Marcelo <marcelomunozp09@gmail.com>
2025-07-06 18:16:33 -0400

    Semana2 CalcularBhaskara

---
+++ Semana2/calcular_bhaskara.py
@@ -0,0 +1,15 @@
+import math
+
+print("Bienvenido a la calculadora de bhaskara!!")
+
+a = float(input("Ingresa el término a: "))
+b = float(input("Ingresa el término b: "))
+c = float(input("Ingresa el término c: "))
+
+x = (b**2)-(4*a*c)
+
+
+x1 = (- b + math.sqrt(x)) / (2*a)
+x2 = (- b - math.sqrt(x)) / (2*a)
+
+print(f"Los resultados son x1 = {x1} y x2 = {x2}")
