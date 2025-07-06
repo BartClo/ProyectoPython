1063f8a580c7553350d020ab8cdee49670e1aebd
Marcelo <marcelomunozp09@gmail.com>
2025-07-06 17:48:54 -0400

    Semana2 Math

--- Semana2/math.py
+++ Semana2/math.py
@@ -7,7 +7,7 @@ w = -4
 #resultado = abs(w) #valor absoluto
 #resultado = pow(4, 3) #4 elevado a 3
 #resultado = max(x, y, z) #Valor máximo de la lista
-#resultado = min(x, y, z) #Valor mínimo de la lista
+resultado = min(x, y, z) #Valor mínimo de la lista
 
 
 print(resultado)
