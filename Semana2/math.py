a1e039ff35f53b129a9ae37c7eb6fdd5590a7482
Marcelo <marcelomunozp09@gmail.com>
2025-07-06 17:48:11 -0400

    Semana2 Math

---
+++ Semana2/math.py
@@ -0,0 +1,13 @@
+x = 3.14
+y = 4
+z = 5
+w = -4
+
+#resultado = round(x)  #redondea
+#resultado = abs(w) #valor absoluto
+#resultado = pow(4, 3) #4 elevado a 3
+#resultado = max(x, y, z) #Valor máximo de la lista
+#resultado = min(x, y, z) #Valor mínimo de la lista
+
+
+print(resultado)
