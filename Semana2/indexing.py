fe9e13e2e22027bcedff6feec4c962fee9a31335
Marcelo <marcelomunozp09@gmail.com>
2025-07-07 18:21:07 -0400

    Semana2 Indexing

---
+++ Semana2/indexing.py
@@ -0,0 +1,11 @@
+# Acceder a algun elementro de una secuencia, usando []
+#[start : end : step]
+
+nombre = "Marcelo Muñoz"
+
+print(nombre[0])
+print(nombre[-1])
+print(nombre[5:])
+print(nombre[0:4])
+print(nombre[::2])
+print(nombre[::-1])
