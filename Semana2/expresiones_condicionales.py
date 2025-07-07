85c9959dc5536ad33ecb6863c5cde7b28d9dfb13
Marcelo <marcelomunozp09@gmail.com>
2025-07-07 17:41:51 -0400

    Semana2 ExpresionesCondicionales

---
+++ Semana2/expresiones_condicionales.py
@@ -0,0 +1,17 @@
+#atajo para IF-ELSE
+#Print o asigna 1 o 2 valores basados en una condición
+#formula  -->  X if condicion else Y
+
+num = 6
+num2 = -4
+edad = 21
+
+print("Positivo" if num >=0 else "Negativo")
+print("Positivo" if num2 >=0 else "Negativo")
+
+result = "Par" if num % 2 == 0 else "Impar"
+print(result)
+
+
+estado = "Adulto" if edad >= 18 else "Menor de edad"
+print(estado)
