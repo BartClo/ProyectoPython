69d732f83f038e03cd39f38a76995c79be3b2e15
Marcelo <marcelomunozp09@gmail.com>
2025-07-10 21:55:57 -0400

    Semana2 Ejercicio

---
+++ Semana2/Ejercicios/sumar_sin_multiplos.py
@@ -0,0 +1,7 @@
+#Usa un for para imprimir los números del 1 al 20, pero usa continue para saltar los múltiplos de 3.
+
+for x in range(0, 21):
+    if x % 3 == 0:
+        continue
+    else:
+        print(x)
