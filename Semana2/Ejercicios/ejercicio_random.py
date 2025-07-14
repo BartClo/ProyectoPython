02324d4323194759f6ec189ef76be845634abc49
Marcelo <marcelomunozp09@gmail.com>
2025-07-13 21:02:08 -0400

    Semana2 Ejercicio

--- Semana2/Ejercicios/ejercicio_random.py
+++ Semana2/Ejercicios/ejercicio_random.py
@@ -13,3 +13,10 @@ else:
     print("GG WP!!")
 
 
+#El programa elige un número aleatorio entre 1 y 100. El usuario debe adivinarlo.
+
+#Si el número es muy bajo, le dice "Muy bajo".
+
+#Si es muy alto, "Muy alto".
+
+#Termina con break si lo adivina.
