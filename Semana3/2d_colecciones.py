2d982fd673cd85602ec2e822aecbb2dc57fa750f
Marcelo <marcelomunozp09@gmail.com>
2025-07-27 17:57:24 -0400

    Semana3 2DColecciones

---
+++ Semana3/2d_colecciones.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "orange", "banana", "coconut"]
+vegetables = ["celery", "carrots", "potatoes"]
+meats = ["chicken", "fish", "turkey"]
+
+groceries = [fruits, vegetables, meats]  #2d list
+
+print(groceries[0][1])  #primero declaras la lista, luego el objeto de la lista
