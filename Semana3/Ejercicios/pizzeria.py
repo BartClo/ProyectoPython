eb50920c1ce6586fc872cc6e991f339a013a9930
Marcelo <marcelomunozp09@gmail.com>
2025-08-11 23:09:26 -0400

    Semana3 Ejercicio Incompleto

--- Semana3/Ejercicios/pizzeria.py
+++ Semana3/Ejercicios/pizzeria.py
@@ -1,3 +1,5 @@
+import time
+
 print("-> Pizzería PF <-")
 
 dinero = float(input("Ingrese el dinero disponible para pagar: "))
@@ -6,41 +8,47 @@ pizzas = {"Napolitana": 1400,
           "Pepperoni": 1100,
           "4 Quesos": 1350,
           "Margarita": 1600}
-extras = {}
-print(pizzas)
-keys = pizzas.keys()
-values = pizzas.values()
-print(keys)
-print(values)
+extras = []
+
 print("Nuestro catalogo de pizzas son las siguientes:\nNapolitana -> $1400 \nPepperoni -> $1100 \n4 Quesos -> $1350\nMargarita -> $1600")
 compra = input("Elija su pizza (Nombre completo e igual): ")
 
-keys = pizzas.keys()
-values = pizzas.values()
-print(keys)
+
 while compra not in pizzas.keys():
     print("No existe de esa pizza")
     compra = input("Elija su pizza (Nombre completo e igual): ")
 else:
-    while dinero in pizzas.values():
-        
-        if dinero < pizzas.values(compra):
-            print("No tienes suficiente dinero")
+    precio_pizza = pizzas[compra]
+    if dinero >= precio_pizza:
+        print(f"Has seleccionado {compra}")
+        time.sleep(2)
+        x = input("Desea agregar extras a la pizza (Sí o No): ")
+        if x == "Sí":
+            while True:
+                extras.append(input("Ingrese los ingredientes extras (Cada extra cuesta $300) presiona q para terminar: "))
+                if extras.append is "q":
+                    break
+                else:
+                    for extra in extras:
+                        precio_pizza += 300
+                        if precio_pizza > dinero:
+                            print("No tienes dinero suficiente para agregar ingredientes")
+                        else:
+                            print(f"El total a pagar por la {compra} con extra {extras} es ${precio_pizza}.\n El pago a sido realizado el vuelto es de ${dinero - precio_pizza}")
         else:
-            print(f"Has seleccionado {compra}")
-            
-x = input("Desea agregar extras a la pizza (Sí o No): ")
-
-if x is "Sí":
-    extras.append(input("Ingrese los ingredientes extras: "))
-else:
-    print(f"El total a pagar por la {pizzas} es ${pizzas.values(compra)}.\n El pago a sido realizado el vuelto es de ${pizzas.values(compra) - dinero }")
-    
-            
+            print(f"El total a pagar por la {compra} es ${precio_pizza}.\n El pago a sido realizado el vuelto es de ${dinero - precio_pizza}")
+    else:
+        print("No tienes suficiente dinero")
+        
 
 
 
 
-    
+
+
+
+
+
+
 
 
